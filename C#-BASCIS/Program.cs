a4674d576648b334127261170d5b85799290d6dc
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-01-16 19:39:36 +0530

    Initial commit--first code

---
+++ C#-BASCIS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__BASCIS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("THIS FOR BASIC LEARNINGS");
+            Console.ReadLine();
+        }
+    }
+}
