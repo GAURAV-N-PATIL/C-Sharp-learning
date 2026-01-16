930961b0910d0a227db589d7f0840dd2ee2df7a0
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-01-16 23:51:43 +0530

    Comments in C# is done

--- C#-BASCIS/Program.cs
+++ C#-BASCIS/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using System; //this means we are using class from the system namespace
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C__BASCIS
+namespace C_sharp_BASCIS //namespace is used to organize code and is a container for classes and other namespaces
 {
-    internal class Program
+    public class Program //class is conatiner for data and methods,which brings functionality to the program
     {
         static void Main(string[] args)
         {
+            //C# is a programming language developed by Microsoft that runs on the .NET Framework.
+            //it is used to developed web apps, desktop apps, mobile apps, games and more.
+            //it is Object Oriented Programming Language. It is close to C++ and Java.
             Console.WriteLine("Hello, World!");
             Console.WriteLine("THIS FOR BASIC LEARNINGS");
             Console.ReadLine();
